509a0c5dd4c604bd1f79a4516e6846e897e4458f
MarcosRodriguez03 <marcos.e.rodriguez2003@gmail.com>
2024-01-26 14:54:54 -0800

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,14 @@
 const call = async (name) => {
     const promise = await fetch('https://allforonehosting.azurewebsites.net/controller/SayHell/' + name);
     const data = await promise.text();
-
-    console.log(data)
     return data;
-
 }
-
 let p2Text = document.getElementById("p2Text")
 let p2Enter = document.getElementById("p2Enter")
 let p2Input = document.getElementById("p2Input")
 if (p2Text) {
     p2Input.addEventListener('keydown', async (event) => {
         if (p2Input.value === '') {
-            console.log("fsdfsfdsfd")
         } else {
             if (event.key === 'Enter') {
                 let word = await call(event.target.value)
@@ -21,43 +16,33 @@ if (p2Text) {
                 p2Input.value = "";
             }
         }
-
-
     })
     p2Enter.addEventListener('click', async () => {
         if (p2Input.value === '') {
-            console.log("fsdfsfdsfd")
+
         } else {
             let word = await call(p2Input.value)
             p2Text.textContent = word;
             p2Input.value = "";
         }
-
     })
 }
-
-
 // -------------------------------------------------------
 
 let p3Text = document.getElementById("p3Text")
 let p3Enter = document.getElementById("p3Enter")
 let p3Input1 = document.getElementById("p3Input1")
 let p3Input2 = document.getElementById("p3Input2")
-
 const call3 = async (number1, number2) => {
     const promise = await fetch(`https://allforonehosting.azurewebsites.net/Add2Numbers/Adding2Num/${number1}/${number2}`);
     const data = await promise.text();
-
-    console.log(data)
     return data;
-
 }
 if (p3Text) {
     p3Enter.addEventListener('click', async () => {
         if (p3Input1.value === '' || p3Input2.value === "") {
-            console.log("fsdfsfdsfd")
         } else {
-            console.log("Sf")
+
             let word = await call3(p3Input1.value, p3Input2.value)
             p3Text.textContent = word;
             p3Input1.value = "";
@@ -76,23 +61,17 @@ let p4Input2 = document.getElementById("p4Input2")
 const call4 = async (number1, number2) => {
     const promise = await fetch(`https://allforonehosting.azurewebsites.net/Ask2Questions/Ask2Questions/${number1}/${number2}`);
     const data = await promise.text();
-
-    console.log(data)
     return data;
-
 }
 if (p4Text) {
     p4Enter.addEventListener('click', async () => {
         if (p4Input1.value === '' || p4Input2.value === "") {
-            console.log("fsdfsfdsfd")
         } else {
-            console.log("Sf")
             let word = await call4(p4Input1.value, p4Input2.value)
             p4Text.textContent = word;
             p4Input1.value = "";
             p4Input2.value = "";
         }
-
     })
 }
 //-----------------------------------------------------------
@@ -100,27 +79,20 @@ let p5Text = document.getElementById("p5Text")
 let p5Enter = document.getElementById("p5Enter")
 let p5Input1 = document.getElementById("p5Input1")
 let p5Input2 = document.getElementById("p5Input2")
-
 const call5 = async (number1, number2) => {
     const promise = await fetch(`https://allforonehosting.azurewebsites.net/LessOrGreater/LessOrGreater/${number1}/${number2}`);
     const data = await promise.text();
-
-    console.log(data)
     return data;
-
 }
 if (p5Text) {
     p5Enter.addEventListener('click', async () => {
         if (p5Input1.value === '' || p5Input2.value === "") {
-            console.log("fsdfsfdsfd")
         } else {
-            console.log("Sf")
             let word = await call5(p5Input1.value, p5Input2.value)
             p5Text.textContent = word;
             p5Input1.value = "";
             p5Input2.value = "";
         }
-
     })
 }
 //----------------------------------
@@ -129,10 +101,7 @@ if (p5Text) {
 const call6 = async (gender, name, age, color, pronoun, food, job, adjective1, adjective2, noun) => {
     const promise = await fetch(`https://allforonehosting.azurewebsites.net/MadLib/MadLib/${gender}/${name}/${age}/${color}/${pronoun}/${food}/${job}/${adjective1}/${adjective2}/${noun}`);
     const data = await promise.text();
-
-    console.log(data)
     return data;
-
 }
 
 let p6Text = document.getElementById("p6Text")
@@ -152,9 +121,9 @@ let p6Input10 = document.getElementById("p6Input10")
 if (p6Text) {
     p6Enter.addEventListener('click', async () => {
         if (p6Input1.value === '' || p6Input2.value === "" || p6Input2.value === "" || p6Input3.value === "" || p6Input4.value === "" || p6Input5.value === "" || p6Input6.value === "" || p6Input7.value === "" || p6Input8.value === "" || p6Input9.value === "" || p6Input10.value === "") {
-            console.log("fsdfsfdsfd")
+
         } else {
-            console.log("Sf")
+
             let word = await call6(p6Input1.value, p6Input2.value, p6Input3.value, p6Input4.value, p6Input5.value, p6Input6.value, p6Input7.value, p6Input8.value, p6Input9.value, p6Input10.value)
             p6Text.textContent = word;
             p6Input1.value = "";
@@ -173,56 +142,13 @@ if (p6Text) {
     })
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //----------------------------------
 
 const call7 = async (name) => {
     const promise = await fetch(`https://allforonehosting.azurewebsites.net/ReverseItAlphanumeric/ReverseItAlphanumeric/${name}`);
     const data = await promise.text();
 
-    console.log(data)
+
     return data;
 
 }
@@ -239,7 +165,7 @@ if (p7Text) {
 
 
         if (p7Input.value === '') {
-            console.log("fsdfsfdsfd")
+
         } else {
             if (event.key === 'Enter') {
                 let word = await call7(event.target.value)
@@ -253,7 +179,7 @@ if (p7Text) {
     })
     p7Enter.addEventListener('click', async () => {
         if (p7Input.value === '') {
-            console.log("fsdfsfdsfd")
+
         } else {
             let word = await call7(p7Input.value)
             p7Text.textContent = word;
@@ -269,7 +195,7 @@ const call8 = async (name) => {
     const promise = await fetch(`https://allforonehosting.azurewebsites.net/ReverseItNumbersOnly/ReverseItNumbersOnly/${name}`);
     const data = await promise.text();
 
-    console.log(data)
+
     return data;
 
 }
@@ -283,7 +209,7 @@ if (p8Text) {
     p8Input.addEventListener('keydown', async (event) => {
 
         if (p8Input.value === '') {
-            console.log("fsdfsfdsfd")
+
         } else {
             if (event.key === 'Enter') {
                 let word = await call8(event.target.value)
@@ -294,7 +220,7 @@ if (p8Text) {
     })
     p8Enter.addEventListener('click', async () => {
         if (p8Input.value === '') {
-            console.log("fsdfsfdsfd")
+
         } else {
             let word = await call8(p8Input.value)
             p8Text.textContent = word;
@@ -308,7 +234,7 @@ const call9 = async (name) => {
     const promise = await fetch(`https://allforonehosting.azurewebsites.net/RestaurantPicker/RestaurantPicker 1.Japanese 2.Korean 3.Chinese/${name}`);
     const data = await promise.text();
 
-    console.log(data)
+
     return data;
 
 }
@@ -322,7 +248,7 @@ if (p9Text) {
     p9Input.addEventListener('keydown', async (event) => {
 
         if (p9Input.value === '') {
-            console.log("fsdfsfdsfd")
+
         } else {
             if (event.key === 'Enter') {
                 let word = await call9(event.target.value)
@@ -333,7 +259,7 @@ if (p9Text) {
     })
     p9Enter.addEventListener('click', async () => {
         if (p9Input.value === '') {
-            console.log("fsdfsfdsfd")
+
         } else {
             let word = await call9(p9Input.value)
             p9Text.textContent = word;
@@ -349,7 +275,7 @@ const call10 = async () => {
     const promise = await fetch(`https://allforonehosting.azurewebsites.net/Magic8Ball/Magic8Ball`);
     const data = await promise.text();
 
-    console.log(data)
+
     return data;
 
 }
@@ -363,7 +289,7 @@ if (p10Text) {
     p10Input.addEventListener('keydown', async (event) => {
 
         if (p10Input.value === '') {
-            console.log("fsdfsfdsfd")
+
         } else {
             if (event.key === 'Enter') {
                 let word = await call10(event.target.value)
@@ -374,7 +300,7 @@ if (p10Text) {
     })
     p10Enter.addEventListener('click', async () => {
         if (p10Input.value === '') {
-            console.log("fsdfsfdsfd")
+
         } else {
             let word = await call10(p10Input.value)
             p10Text.textContent = word;
@@ -389,7 +315,7 @@ const call11 = async (name) => {
     const promise = await fetch(`https://allforonehosting.azurewebsites.net/OddOrEven/OddOrEven/${name}`);
     const data = await promise.text();
 
-    console.log(data)
+
     return data;
 
 }
@@ -403,7 +329,7 @@ if (p11Text) {
     p11Input.addEventListener('keydown', async (event) => {
 
         if (p11Input.value === '') {
-            console.log("fsdfsfdsfd")
+
         } else {
             if (event.key === 'Enter') {
                 let word = await call11(event.target.value)
@@ -414,7 +340,7 @@ if (p11Text) {
     })
     p11Enter.addEventListener('click', async () => {
         if (p11Input.value === '') {
-            console.log("fsdfsfdsfd")
+
         } else {
             let word = await call11(p11Input.value)
             p11Text.textContent = word;
